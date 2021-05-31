3024f7c918dc079c4adea7a356d64b2181b81aa5
isrt09 <mazedur@gmail.com>
2021-05-31 20:13:55 +0600

    Series attributes & methods

--- Series.py
+++ Series.py
@@ -22,3 +22,39 @@ pd.Series(5, index=[0])
 pd.Series(5, index=[0,1,2,3,4,5])
 pd.Series([2 * num for num in range(10)])
 [2 * num for num in range(10)]
+
+
+# Series Attributes and Mehtods
+data = pd.read_csv('Stock.csv')
+data = pd.read_csv('Stock.csv', usecols=['Open'])
+data = pd.read_csv('Stock.csv', usecols=['Open'], squeeze=True)
+data.index
+data.values
+data.dtype
+data.ndim
+data.size
+data.is_unique
+data.is_monotonic_increasing
+data.add_prefix('product_')
+data.add_suffix('_product')
+data.idxmax()
+data.idxmin()
+data.sum()
+data.mean()
+data.std()
+data.var()
+data.min()
+data.max()
+data.product()
+data.count()
+data.describe()
+
+temperature = [30,20,45,30,29,30,35]
+weeks       = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+pd.Series(temperature, weeks)
+pd.Series(data=temperature, index = weeks)
+s = pd.Series(data=temperature, index = weeks)
+type(s)
+s.dtypes
+s.index
+s.values
